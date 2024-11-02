d3f02b85ae0ee3051ec4a4a09d247eefa842c3d7
shehan2000 <maliths.20@cse.mrt.ac.lk>
2024-11-03 04:10:07 +0530

    logo and the name added and make it responsive

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export default function DashboardLayout({
     children,
   }: Readonly<{
@@ -7,7 +10,13 @@ export default function DashboardLayout({
     
         <div className="h-screen flex">
           {/* LEFT */}
-          <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200" >l</div>
+          <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200 p-4" >
+            <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
+              <Image src="/lmslogo.png" alt='logo' width={64} height={32} />
+              <span className="hidden lg:block">Simple LMS</span>
+            </Link>
+            </div>
+          
           {/* RIGHT */}
           <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">r</div>
         </div>
