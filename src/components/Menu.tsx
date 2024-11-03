284a4a910750f5e768b4179b0a7be2b67eb5d5d8
shehan2000 <maliths.20@cse.mrt.ac.lk>
2024-11-03 22:37:36 +0530

    created and make responsive a simple menu item

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const menuItems = [
   {
     title: "MENU",
@@ -112,3 +115,25 @@ const menuItems = [
     ],
   },
 ];
+const Menu = () => {
+  return (
+    <div className='mt-4 text-sm'>
+      {menuItems.map((i) => 
+      (<div className="flex flex-col gap-1" key={i.title}>
+        <span className="hidden lg:block text-gray-400 font-light my-4" >
+          {i.title}
+        </span>
+        {i.items.map((item)=>(
+          <Link href={item.href} key={item.label} className="flex items-center justify-center lg:justify-start gap-2 text-gray-500 py-1">
+          <Image src={item.icon}
+          alt="" width={16} height={16} />
+            <span className="hidden lg:block">{item.label}</span>
+          </Link>
+        ))}
+        </div>)
+        )}
+    </div>
+  )
+}
+
+export default Menu
