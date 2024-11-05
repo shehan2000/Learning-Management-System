a5c94010fc47cd3f5c5dbae7fc8836b4222c098a
shehan2000 <maliths.20@cse.mrt.ac.lk>
2024-11-05 21:53:16 +0530

    basic structure of the admin page

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,6 +1,11 @@
     const AdminPage = () => {
       return (
-        <div className=''>AdminPage</div>
+        <div className='p-4 flex gap-4 flex-col md:flex-row'>
+          {/*LEFT SIDE*/}
+          <div className='w-full lg:w-2/3'>l</div>
+          {/*RIGHT SIDE*/}
+          <div className='w-full lg:w-1/3'>r</div>
+        </div>
       )
     }
     
