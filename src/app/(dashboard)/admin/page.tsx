9a24f13a87361f20e5938c7b9b7ef6fb421d5b6d
shehan2000 <maliths.20@cse.mrt.ac.lk>
2024-11-12 23:25:06 +0530

    added user cards to the admin page

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,12 +1,22 @@
-    const AdminPage = () => {
-      return (
-        <div className='p-4 flex gap-4 flex-col md:flex-row'>
-          {/*LEFT SIDE*/}
-          <div className='w-full lg:w-2/3'>l</div>
-          {/*RIGHT SIDE*/}
-          <div className='w-full lg:w-1/3'>r</div>
+import UserCard from "@/components/UserCard"
+
+const AdminPage = () => {
+  return (
+    <div className='p-4 flex gap-4 flex-col md:flex-row'>
+      {/*LEFT SIDE*/}
+      <div className='w-full lg:w-2/3'>
+      {/*USER CARDS*/}
+      <div className='flex gap-4 justify-between'>
+        <UserCard type='Students' />
+        <UserCard type='Teachers' />
+        <UserCard type='Parents' />
+        <UserCard type='Staffs' />
         </div>
-      )
-    }
-    
-    export default AdminPage;
+      </div>
+      {/*RIGHT SIDE*/}
+      <div className='w-full lg:w-1/3'>r</div>
+    </div>
+  )
+}
+
+export default AdminPage;
