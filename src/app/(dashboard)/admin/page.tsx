a6375baa8a23b4990d28105e1c936357d82eff4b
shehan2000 <maliths.20@cse.mrt.ac.lk>
2025-01-06 14:04:27 +0530

    attendence and count charts completed

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,17 +1,37 @@
+
 import UserCard from "@/components/UserCard"
+import CountChart from "@/components/CountChart";
+import AttendanceChart from "@/components/AttendanceChart";
 
 const AdminPage = () => {
   return (
     <div className='p-4 flex gap-4 flex-col md:flex-row'>
       {/*LEFT SIDE*/}
-      <div className='w-full lg:w-2/3'>
+      <div className='w-full lg:w-2/3 flex flex-col gap-8'>
       {/*USER CARDS*/}
       <div className='flex gap-4 justify-between flex-wrap'>
         <UserCard type='Students' />
         <UserCard type='Teachers' />
         <UserCard type='Parents' />
         <UserCard type='Staffs' />
+      </div>
+
+      {/*MIDDLE CHARTS*/}
+      <div className="flex gap-4 flex-col lg:flex-row">
+        {/*COUNT CHART*/}
+        <div className='w-full lg:w-1/3 h-[450px]'>
+          <CountChart />
         </div>
+        {/*ATTENDANCE CHART*/}
+        <div className="w-full lg:w-1/2 h-[450px]">
+          <AttendanceChart />
+        </div>  
+
+      </div>   
+      {/*BOTTOM CHARTS*/}
+      <div>
+
+      </div> 
       </div>
       {/*RIGHT SIDE*/}
       <div className='w-full lg:w-1/3'>r</div>
