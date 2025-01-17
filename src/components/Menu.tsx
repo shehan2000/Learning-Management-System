31e85ece74f6af21e0eedcdb1afa6f16437d9459
shehan2000 <maliths.20@cse.mrt.ac.lk>
2025-01-17 18:20:11 +0530

    table component

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { role } from "@/lib/data";
 
 const menuItems = [
   {
@@ -123,15 +124,22 @@ const Menu = () => {
         <span className="hidden lg:block text-gray-400 font-light my-4" >
           {i.title}
         </span>
-        {i.items.map((item)=>(
-          <Link href={item.href} key={item.label} className="flex items-center justify-center lg:justify-start gap-2 text-gray-500 py-1">
-          <Image src={item.icon}
-          alt="" width={16} height={16} />
-            <span className="hidden lg:block">{item.label}</span>
-          </Link>
-        ))}
-        </div>)
-        )}
+        {i.items.map((item) => {
+            if (item.visible.includes(role)) {
+              return (
+                <Link
+                  href={item.href}
+                  key={item.label}
+                  className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-lamaSkyLight"
+                >
+                  <Image src={item.icon} alt="" width={20} height={20} />
+                  <span className="hidden lg:block">{item.label}</span>
+                </Link>
+              );
+            }
+          })}
+          </div>
+      ))}
     </div>
   )
 }
