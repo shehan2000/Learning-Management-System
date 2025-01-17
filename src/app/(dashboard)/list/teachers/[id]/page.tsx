21404903495a76d5ba2210a372d6a67279ef57d8
shehan2000 <maliths.20@cse.mrt.ac.lk>
2025-01-17 18:22:16 +0530

    feat: update dependencies and add TeacherListPage with calendar and announcements

---
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='p-4 flex items-center justify-between text-gray-500'>
+      
+    </div>
+  )
+}
+
+export default page
