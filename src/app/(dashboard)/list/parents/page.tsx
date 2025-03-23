3b460b8dfb8f7b1aaf4c5068df879f199e7f43e8
shehan2000 <maliths.20@cse.mrt.ac.lk>
2025-03-23 12:18:51 +0530

    feat: enhance Teacher and Subject list pages with search functionality and pagination support

--- src/app/(dashboard)/list/parents/page.tsx
+++ src/app/(dashboard)/list/parents/page.tsx
@@ -3,17 +3,21 @@ import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
 import { parentsData, role } from "@/lib/data";
+import { prisma } from "@/lib/prisma";
+import { ITEM_PER_PAGE } from "@/lib/settings";
+import { Parent, Prisma, Student } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
 
-type Parent = {
-  id: number;
-  name: string;
-  students: string[];
-  phone: string;
-  email?:string;
-  address: string;
-};
+// type Parent = {
+//   id: number;
+//   name: string;
+//   students: string[];
+//   phone: string;
+//   email?:string;
+//   address: string;
+// };
+type ParentList=Parent & {students:Student[]};
 
 const columns = [
   {
@@ -40,40 +44,77 @@ const columns = [
     accessor: "action",
   },
 ];
+const renderRow = (item: ParentList) => (
+  <tr
+    key={item.id}
+    className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
+  >
+    <td className="flex items-center gap-4 p-4">
+      
+      <div className="flex flex-col">
+        <h3 className="font-semibold">{item.name}</h3>
+        <p className="text-xs text-gray-500">{item?.email}</p>
+      </div>
+    </td>
+    <td className="hidden md:table-cell">{item.students?.map(student => student.name).join(", ") ?? ""}</td>
+    <td className="hidden md:table-cell">{item.phone}</td>
+    <td className="hidden md:table-cell">{item.address}</td>
+    <td>
+      <div className="flex items-center gap-2">
+        <Link href={`/list/teachers/${item.id}`}>
+          <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
+            <Image src="/view.png" alt="" width={16} height={16} />
+          </button>
+        </Link>
+        {role === "admin" && (
+          // <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
+          //   <Image src="/delete.png" alt="" width={16} height={16} />
+          // </button>
+          <FormModal table="student" type="delete" id={item.id}/>
+        )}
+      </div>
+    </td>
+  </tr>
+);
 
-const ParentListPage = () => {
-  const renderRow = (item: Parent) => (
-    <tr
-      key={item.id}
-      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
-    >
-      <td className="flex items-center gap-4 p-4">
-        
-        <div className="flex flex-col">
-          <h3 className="font-semibold">{item.name}</h3>
-          <p className="text-xs text-gray-500">{item?.email}</p>
-        </div>
-      </td>
-      <td className="hidden md:table-cell">{item.students.join(",")}</td>
-      <td className="hidden md:table-cell">{item.phone}</td>
-      <td className="hidden md:table-cell">{item.address}</td>
-      <td>
-        <div className="flex items-center gap-2">
-          <Link href={`/list/teachers/${item.id}`}>
-            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
-              <Image src="/view.png" alt="" width={16} height={16} />
-            </button>
-          </Link>
-          {role === "admin" && (
-            // <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
-            //   <Image src="/delete.png" alt="" width={16} height={16} />
-            // </button>
-            <FormModal table="student" type="delete" id={item.id}/>
-          )}
-        </div>
-      </td>
-    </tr>
-  );
+const ParentListPage = async({
+  searchParams}: {searchParams: {[key: string]: string | undefined};
+}) => {
+  console.log(searchParams);
+  const {page, ...queryParams} = searchParams;
+  const p= page ? parseInt(page) : 1;
+
+  //URL PARAMS CONDITION
+  const query:Prisma.ParentWhereInput={};
+  if(queryParams){
+    for(const [key,value] of Object.entries(queryParams)){
+      if(value!==undefined){
+        switch(key){
+          case "search":
+            query.name={contains:value,mode:"insensitive"};
+            break ;
+          default:
+            break;  
+        }
+      }
+    }
+  }
+  
+
+  const[data,count]=await prisma.$transaction([
+    prisma.parent.findMany(
+      {
+        where:query,
+        include:{students:true},
+        take:ITEM_PER_PAGE,
+        skip:(p-1)*ITEM_PER_PAGE,
+      }
+      
+    ),
+    prisma.parent.count({where:query}),
+  ])
+  console.log(data);
+  
 
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
@@ -99,9 +140,9 @@ const ParentListPage = () => {
         </div>
       </div>
       {/* LIST */}
-      <Table columns={columns} renderRow={renderRow} data={parentsData} />
+      <Table columns={columns} renderRow={renderRow} data={data} />
       {/* PAGINATION */}
-      <Pagination />
+      <Pagination page={p} count={count} />
     </div>
   );
 };
--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -5,7 +5,7 @@ import TableSearch from "@/components/TableSearch";
 import { role, teachersData } from "@/lib/data";
 import { prisma } from "@/lib/prisma";
 import { ITEM_PER_PAGE } from "@/lib/settings";
-import { Class, Subject, Teacher } from "@prisma/client";
+import { Class, Prisma, Subject, Teacher } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -107,8 +107,42 @@ const TeacherListPage = async({
   console.log(searchParams.page)
   const {page, ...queryParams}= searchParams
   const p=page ?parseInt(page) : 1;
+
+  //URL PARAMS CONDITION
+
+  const query:Prisma.TeacherWhereInput={}
+
+  if(queryParams){
+    for (const [key, value] of Object.entries(queryParams)) {
+      if(value!==undefined){
+      switch (key) {
+        case "classId":
+            query.lessons= {
+              some:{classId:parseInt(value),
+          
+              },
+            };
+            break;
+        case "search":
+          query.name={
+            contains:value,
+            mode:"insensitive"
+          } 
+          break; 
+        default:
+            break;    
+         
+      }
+    }
+  }
+}
+
+
   const [data,count]=await prisma.$transaction([
     prisma.teacher.findMany({
+    where:
+      query,
+    
     include:{
       subjects:true,
       classes:true
@@ -116,7 +150,10 @@ const TeacherListPage = async({
     take:ITEM_PER_PAGE,
     skip:ITEM_PER_PAGE * (p-1)
   }),
-  prisma.teacher.count()
+  prisma.teacher.count({
+    where:query
+    }
+  ),
 ])
 
  
