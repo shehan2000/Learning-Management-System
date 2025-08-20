c2d85a3fb4ad42f182f063eacd21d61118f0a743
shehan2000 <maliths.20@cse.mrt.ac.lk>
2025-08-20 21:19:22 +0530

    refactor: improve role condition handling and fix operator precedence in ExamListPage

--- src/app/(dashboard)/list/exams/page.tsx
+++ src/app/(dashboard)/list/exams/page.tsx
@@ -60,6 +60,9 @@ const ExamListPage = async({
 
 // ROLE CONDITION
 const roleConditions={
+  admin:{
+
+  },
   teacher:{
     teacherId:currentUserId!
   },
@@ -155,7 +158,7 @@ const renderRow = (item: ExamList) => (
             <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
-            {role === "admin" || role === "teacher" && <FormModal table="exam" type="create" />}
+            {(role === "admin" || role === "teacher") && <FormModal table="exam" type="create" />}
           </div>
         </div>
       </div>
