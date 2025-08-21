2fb663427f149d247145cb8b88c223f8b84dc6f3
shehan2000 <maliths.20@cse.mrt.ac.lk>
2025-08-21 18:52:12 +0530

    refactor: update Navbar to display dynamic user full name instead of static text

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,8 +21,7 @@ const Navbar = async() => {
           <div className='absolute -top-3 -right-3 w-5 h-5 flex items-center justify-center bg-purple-500 text-white rounded-full text-xs'>1</div>
         </div>
       <div className='flex flex-col'>
-        
-        <span className='text-xs leading-3 font-medium'>John Doe</span>
+        <span className='text-xs leading-3 font-medium'>{user?.fullName}</span>
         <span className='text-[10px] text-gray-500 text-right'>{user?.publicMetadata.role as string}</span>
       </div>
       
