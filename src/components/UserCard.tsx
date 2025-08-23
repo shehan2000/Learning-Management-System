7f3e128811f612ef12e9322eeb70c92a2e20fa63
shehan2000 <maliths.20@cse.mrt.ac.lk>
2025-08-23 21:41:34 +0530

    refactor: update UserCard to use specific user types and fetch dynamic data

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,5 +1,16 @@
+import { prisma } from '@/lib/prisma'
 import Image from 'next/image'
-const UserCard = ({type}:{type:string}) => {
+const UserCard = async({type}:{type:"admin"|"student"|"teacher"|"parent"}) => {
+
+  
+  const modelMap:Record<typeof type,any> = {
+    admin: prisma.admin,
+    student:  prisma.student,
+    teacher:  prisma.teacher,
+    parent: prisma.parent,
+  };
+  const data = await modelMap[type].count();
+
   return (
     <div className="rounded-2xl odd:bg-lamaPurple even:bg-lamaYellow p-4 flex-1 min-w-[130px]">
         <div className="flex-justify-between items-center">
@@ -7,7 +18,7 @@ const UserCard = ({type}:{type:string}) => {
             <Image src="/more.png" alt="" width={20} height={20} />
 
         </div>
-        <h1 className='text-2xl font-semibold my-4'>1,234</h1>
+        <h1 className='text-2xl font-semibold my-4'>{data}</h1>
         <h2 className='capitalize text-sm font-medium text-gray-700'>{type}</h2>
     
     </div>
