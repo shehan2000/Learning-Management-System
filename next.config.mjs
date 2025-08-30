417e103cba1a9ce819416d56b48d9ffb07182aed
shehan2000 <maliths.20@cse.mrt.ac.lk>
2025-08-30 18:06:45 +0530

    fix: format next.config.mjs for better readability and maintainability allowed cloudinary hosted images for avoid any unnecassary errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns:[
-            { hostname: "images.pexels.com" },
-            // allow Cloudinary-hosted images
-            { protocol: 'https', hostname: 'res.cloudinary.com', port: '', pathname: '/**' }
-        ]
-    }
+  images: {
+    remotePatterns: [
+      { hostname: "images.pexels.com" },
+      // allow Cloudinary-hosted images
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
